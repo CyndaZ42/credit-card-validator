3e38aedff4ba5b62c375bdb3724a44728a1477b9
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-22 11:17:58 -0700

    should reverse the number
    Co-Authored-By: Anthony DiFalco <106050281+Di-Falco@users.noreply.github.com>

--- src/js/luhn.js
+++ src/js/luhn.js
@@ -2,7 +2,12 @@ export default function Card(number) {
   this.number = number;
 }
 
-Card.prototype.luhn = function() {
-   let cardArray = this.number.split("");
-  return cardArray;
+Card.prototype.split = function() {
+   this.numArray = this.number.split("");
+  return this.numArray;
+}
+
+Card.prototype.reverse = function() {
+  this.numArray.reverse();
+  return this.numArray;
 }
